dac38e38bc621dc3341650c09278b0797fbef7c8
Mark Jason Espelita <mark.medivadigital@gmail.com>
2024-06-30 15:44:44 +0800

    router big letter

--- routes/router.php
+++ routes/router.php
@@ -5,40 +5,39 @@ require '_blackpearl/autoload.php';
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-$routes = new RouteCollection();
+$Routes = new RouteCollection();
 
 // Define you route and controller
 
-$routes->add('welcome', new Route('/', [
+$Routes->add('welcome', new Route('/', [
     '_controller' => 'App\Controller\WelcomeController::index',
 ]));
 
-$routes->add('home', new Route('/home', [
+$Routes->add('home', new Route('/home', [
     '_controller' => 'App\Controller\HomeController::index',
 ]));
 
 
-
 // Auth Routes
 
-$routes->add('register', new Route('/register', [
+$Routes->add('register', new Route('/register', [
     '_controller' => 'App\Controller\AuthController::register',
 ]));
 
-$routes->add('login', new Route('/login', [
+$Routes->add('login', new Route('/login', [
     '_controller' => 'App\Controller\AuthController::login',
 ]));
 
-$routes->add('logout', new Route('/logout', [
+$Routes->add('logout', new Route('/logout', [
     '_controller' => 'App\Controller\AuthController::logout',
 ]));
 
-$routes->add('register-process', new Route('/register-process', [
+$Routes->add('register-process', new Route('/register-process', [
     '_controller' => 'App\Controller\AuthController::registerProcess',
 ]));
 
-$routes->add('login-process', new Route('/login-process', [
+$Routes->add('login-process', new Route('/login-process', [
     '_controller' => 'App\Controller\AuthController::loginProcess',
 ]));
 
-return $routes;
+return $Routes;
